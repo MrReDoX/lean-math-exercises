3146a839a12ba8c95524d971ce7c33d625289e96
tensorix <ivan.ushakov.99@ya.ru>
2026-08-20 15:00:55 +0400

    fields fun

--- Exercises/FieldTheory/01Extensions.lean
+++ Exercises/FieldTheory/01Extensions.lean
@@ -85,22 +85,77 @@ The extension `ℂ / ℝ` has degree `2`.
 
 Prove without using `Complex.finrank_real_complex` or `Complex.basisOneI`. -/
 theorem q4_degree_complex_real : Module.finrank ℝ ℂ = 2 := by
-  sorry
+  have e : ℂ ≃ₗ[ℝ] (Fin 2 → ℝ) := {
+    toFun := fun z ↦ ![z.re, z.im]
+    map_add' := by
+      intro x y
+      simp only [Complex.add_re, Complex.add_im, Matrix.add_cons, Matrix.head_cons,
+        Matrix.tail_cons, Matrix.empty_add_empty]
+    map_smul' := by
+      intro m x
+      simp only [Complex.real_smul, Complex.mul_re, Complex.ofReal_re, Complex.ofReal_im, zero_mul,
+        sub_zero, Complex.mul_im, add_zero, RingHom.id_apply, Matrix.smul_cons, smul_eq_mul,
+        Matrix.smul_empty]
+    invFun := fun v ↦ ⟨v 0, v 1⟩
+    right_inv := by
+      unfold Function.RightInverse
+      unfold Function.LeftInverse
+      intro x
+      simp only [Fin.isValue]
+      ext i
+      fin_cases i <;> simp
+  }
 
+  rw [LinearEquiv.finrank_eq e]
+  exact Module.finrank_fin_fun ℝ
+
+
+example : Module.finrank ℝ ℂ = 2 := by
+  have b : Module.Basis (Fin 2) ℝ ℂ :=
+    Module.Basis.mk (v := ![1, Complex.I])
+      (by
+        rw [@linearIndependent_fin2]
+        simp only [Fin.isValue, Matrix.cons_val_one, Matrix.cons_val_fin_one, ne_eq,
+          Complex.I_ne_zero, not_false_eq_true, Complex.real_smul, Matrix.cons_val_zero, true_and]
+        intro a h
+        rw [Complex.ext_iff] at h
+        simp_all only [Complex.mul_re, Complex.ofReal_re, Complex.I_re, mul_zero, Complex.ofReal_im, Complex.I_im,
+          mul_one, sub_self, Complex.one_re, zero_ne_one, Complex.mul_im, add_zero, Complex.one_im, false_and]
+      )
+      (by
+        simp only [Matrix.range_cons, Matrix.range_empty, Set.union_empty, Set.union_singleton,
+          top_le_iff]
+        rw [@Submodule.eq_top_iff']
+        intro z
+        rw [@Submodule.mem_span_pair]
+        use z.im, z.re
+        rw [Complex.ext_iff]
+        simp only [Complex.real_smul, mul_one, Complex.add_re, Complex.mul_re, Complex.ofReal_re,
+          Complex.I_re, mul_zero, Complex.ofReal_im, Complex.I_im, sub_self, zero_add,
+          Complex.add_im, Complex.mul_im, add_zero, and_self]
+      )
+
+  rw [Module.finrank_eq_card_basis b]
+  simp only [Fintype.card_fin]
 
 /-- **Question 5.**
 
 The polynomial `X² + 1` vanishes at `i`. -/
 theorem q5_i_root :
     (Polynomial.X ^ 2 + Polynomial.C 1 : ℝ[X]).eval₂ (algebraMap ℝ ℂ) Complex.I = 0 := by
-  sorry
+  simp only [map_one, Polynomial.eval₂_add, Polynomial.eval₂_X_pow, Complex.I_sq,
+    Polynomial.eval₂_one, neg_add_cancel]
 
 
 /-- **Question 6.**
 
 The element `i` is integral over `ℝ`. -/
 theorem q6_i_integral : IsIntegral ℝ Complex.I := by
-  sorry
+  use Polynomial.X ^ 2 + Polynomial.C 1
+  simp only [map_one, Polynomial.eval₂_add, Polynomial.eval₂_X_pow, Complex.I_sq,
+    Polynomial.eval₂_one, neg_add_cancel, and_true]
+  unfold Polynomial.Monic
+  simp only [Order.lt_two_iff, zero_le, Polynomial.leadingCoeff_X_pow_add_one]
 
 
 /-- **Question 7.**
@@ -108,7 +163,12 @@ theorem q6_i_integral : IsIntegral ℝ Complex.I := by
 Every `z ∈ ℂ` satisfies `z² - 2 Re(z) z + |z|² = 0`. -/
 theorem q7_complex_quadratic_relation (z : ℂ) :
     z ^ 2 - ((2 * z.re : ℝ) : ℂ) * z + ((Complex.normSq z : ℝ) : ℂ) = 0 := by
-  sorry
+  simp only [Complex.ofReal_mul, Complex.ofReal_ofNat]
+  rw [Complex.normSq_apply, Complex.ext_iff]
+  simp_all only [Complex.ofReal_add, Complex.ofReal_mul, Complex.add_re, Complex.sub_re, Complex.mul_re,
+    Complex.re_ofNat, Complex.ofReal_re, Complex.im_ofNat, Complex.ofReal_im, mul_zero, sub_zero, Complex.mul_im,
+    zero_mul, add_zero, Complex.zero_re, Complex.add_im, Complex.sub_im, Complex.zero_im]
+  apply And.intro <;> simp [pow_two, Complex.mul_re, Complex.mul_im] <;> ring_nf
 
 
 /-- **Question 8.**
